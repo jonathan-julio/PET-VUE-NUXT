37a4f2ec60af911f8d47fa20d433a6ba97a70c8d
˜jonathan-julio˜ <git config --global user.email>
2022-04-06 22:32:11 -0300

    config bootstrap

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class = "container pt-5">
+	    <div class = "mx-auto mt-5">
+        <h1>Bem vindo! Faça login para continuar.</h1>
+        <b-row class = "mx-auto" style="width: 200px;" align-h = "center">
+          <b-button variant = "info" size = "sm" class = "btn btn-primary btn-sm" to = "/loginUsuario">
+            Área do Usuario!
+          </b-button>
+        </b-row> 
+	    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ plugins/bootstrap-vue.js
@@ -0,0 +1,7 @@
+//import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
+//export default bootstrap;
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
